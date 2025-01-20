8683d6de30500d9fe775b38776a1ac45f2555fbc
jupabego97 <juanpablobg97@gmail.com>
2025-01-20 03:25:29 +0000

    finalmente lo dejé en base

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import whisper
 
 # Load the Whisper model once at startup (choose your preferred model size)
-model = whisper.load_model("large")
+model = whisper.load_model("base")
 
 def main():
     st.title("Transcripcion de audio para albita")
