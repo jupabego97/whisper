f80893868e3f9733b9334656a1b9b790dcbf3914
jupabego97 <juanpablobg97@gmail.com>
2025-01-20 03:20:49 +0000

    cambie el modelo a whisper turbo

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ import streamlit as st
 import whisper
 
 # Load the Whisper model once at startup (choose your preferred model size)
-model = whisper.load_model("base")
+model = whisper.load_model("turbo")
 
 def main():
-    st.title("Audio Transcriber with Whisper")
-    st.write("Upload an audio file and let Whisper transcribe it for you.")
+    st.title("Transcripcion de audio para albita")
+    st.write("Suba el audio que quiere transcirbir")
     
     # Create a file uploader for audio files
-    audio_file = st.file_uploader("Upload an Audio File", type=["mp3", "wav", "m4a", "flac", "ogg"])
+    audio_file = st.file_uploader("Suba al archivo de audio", type=["mp3", "wav", "m4a", "flac", "ogg"])
 
     if audio_file is not None:
         # Save the uploaded file to a temporary location
         with open("temp_audio_file", "wb") as f:
             f.write(audio_file.getbuffer())
         
-        st.info("Transcribing... This may take a moment.")
+        st.info("Transcribiendo... esto se demora, pero funciona,tengale paciencia")
 
         # Use the Whisper model to transcribe
         result = model.transcribe("temp_audio_file")
